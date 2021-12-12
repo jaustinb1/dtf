2e5403839a5f15ab75869d3c13e01c41b8ca4b6a
Jonathan Booher <jaustinb@stanford.edu>
2021-12-11 19:13:44 -0700

    allow matrix and push to single sink

--- dtf/modules/distributed_module.py
+++ dtf/modules/distributed_module.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import time
+import numpy as np
 
 class DistributedModel:
     def __init__(self, base_model,
@@ -8,27 +9,41 @@ class DistributedModel:
                  sink=None,
                  mode=None,
                  index=None,
-                 multi_sink=False,
-                 storage_spec=None):
+                 storage_spec=None,
+                 push_to_all=False):
         self._base_model = base_model
         self._cluster = cluster
-        self._multi_sink = multi_sink
+        self._multi_sink = cluster.count(sink) > 1
+        self._multi_source = cluster.count(source) > 1
         self._storage_spec = storage_spec
+        self.is_source = mode == source
+        self.source = source
+        self.sink = sink
+        self._push_to_all = push_to_all
+        self._matrixed = self._multi_sink and self._multi_source
 
-        if multi_sink:
-            # If we are a multisink, we assume only one source
-            self.update_device = self._cluster.get_device(source)
+        if self._multi_sink and self._multi_source:
+            self._num_replicas = cluster.count(sink) * cluster.count(source)
+            self.update_devices = []
+            for _ in range(cluster.count(source)):
+                buff = []
+                for sink_ind in range(cluster.count(sink)):
+                    buff.append(self._cluster.get_device(sink, sink_ind))
+                self.update_devices.append(buff)
+        elif self._multi_sink:
+            # Single source, multiple sinks
+            assert not self._multi_source
             self._num_replicas = cluster.count(sink)
+            self.update_devices = [[
+                self._cluster.get_device(source)] * self._num_replicas]
         else:
-            # If we are not multisink, we are multisource and index is
-            # the source index.
-            self.update_device = self._cluster.get_device(sink)
+            # Multiple sources, single sink
+            assert not self._multi_sink
             self._num_replicas = cluster.count(source)
+            self.update_devices = [
+                [self._cluster.get_device(sink)]]*self._num_replicas
 
         self.update_name = f"{source}->{sink}"
-
-        self.is_source = mode == source
-
         self.index = index
 
         self._update_queues = {}
@@ -42,67 +57,103 @@ class DistributedModel:
         return self._base_model.variables
 
     def _make_update(self):
-        with tf.device(self.update_device):
-            if self._multi_sink:
-                variables = self.variables
-                dtypes = [v.dtype for v in variables]
-                shapes = [v.shape for v in variables]
-                names = [v.name for v in variables]
-            else:
-                dtypes = self._storage_spec['dtypes']
-                shapes = self._storage_spec['shapes']
-                names = self._storage_spec['names']
+        if self._multi_sink and not self._matrixed:
+            variables = self.variables
+            dtypes = [v.dtype for v in variables]
+            shapes = [v.shape for v in variables]
+            names = [v.name for v in variables]
+        else:
+            dtypes = self._storage_spec['dtypes']
+            shapes = self._storage_spec['shapes']
+            names = self._storage_spec['names']
 
-            tag = [f"({i})" for i in range(self._num_replicas)]
-            if self._multi_sink:
-                name_fn = lambda x: self.update_name + x
-            else:
-                name_fn = lambda x: x + self.update_name
+        tag = [f"({i})" for i in range(self._num_replicas)]
+        tag = []
+        for source_ind in range(self._cluster.count(self.source)):
+            buff = []
+            for sink_ind in range(self._cluster.count(self.sink)):
+                buff.append(f"({source_ind},{sink_ind})")
+            tag.append(buff)
 
-            for s in tag:
-                self._update_queues[s] = tf.queue.FIFOQueue(
-                    capacity=1, dtypes=dtypes,
-                    shapes=shapes, names=names,
-                    shared_name=name_fn(s),
-                    name=name_fn(s))
+        if self._multi_sink:
+            name_fn = lambda x: self.update_name + x
+        else:
+            name_fn = lambda x: x + self.update_name
 
-    def push(self, data=None):
+        for source_ind in range(len(tag)):
+            for sink_ind in range(len(tag[source_ind])):
+                with tf.device(self.update_devices[source_ind][sink_ind]):
+                    s = tag[source_ind][sink_ind]
+                    self._update_queues[s] = tf.queue.FIFOQueue(
+                        capacity=1, dtypes=dtypes,
+                        shapes=shapes, names=names,
+                        shared_name=name_fn(s),
+                        name=name_fn(s))
+
+    def push(self, data=None, force_ind=None):
         assert self.is_source
 
-        for i in range(self._num_replicas):
-            s = f"({i})"
-            if not self._multi_sink and i != self.index:
-                continue
-            if data:
-                self._update_queues[s].enqueue({
-                    k: v for k, v in data.items()
-                })
+        if not self._multi_sink or self._push_to_all:
+            # single sink or multi sink but pushing updates to all
+            sink_id = range(self._cluster.count(self.sink))
+        else:
+            assert not self._push_to_all
+            if force_ind is not None:
+                sink_id = [force_ind]
             else:
-                self._update_queues[s].enqueue({
-                    v.name: v for v in self.variables
-                })
-        print("Pushed Updates")
+                sink_id = [np.random.choice(
+                    range(self._cluster.count(self.sink)))]
+        for source_ind in range(self._cluster.count(self.source)):
+            if source_ind != self.index:
+                continue
+            for sink_ind in sink_id:
+                s = f"({source_ind},{sink_ind})"
+                if data:
+                    self._update_queues[s].enqueue({
+                        k: v for k, v in data.items()
+                    })
+                else:
+                    self._update_queues[s].enqueue({
+                        v.name: v for v in self.variables
+                    })
+        print("Pushed all updates")
 
     def maybe_pull(self):
         assert not self.is_source
-        s = f"({self.index})"
-        if self._update_queues[s].size() > 0:
-            update = self._update_queues[s].dequeue()
-            for i in range(len(self.variables)):
-                name = self.variables[i].name
-                self.variables[i].assign(update[name])
-            print("Successfully pulled update")
-        else:
-            print("Nothing to pull")
+
+        for source_ind in range(self._cluster.count(self.source)):
+            for sink_ind in range(self._cluster.count(self.sink)):
+                if sink_ind != self.index:
+                    continue
+                s = f"({source_ind},{sink_ind})"
+                if self._update_queues[s].size() > 0:
+                    update = self._update_queues[s].dequeue()
+                    for i in range(len(self.variables)):
+                        name = self.variables[i].name
+                        self.variables[i].assign(update[name])
+                    print("Successfully pulled update")
+                    return
+                else:
+                    print("Nothing to pull")
 
     def pull(self):
         assert not self.is_source
-        s = f"({self.index})"
-        while self._update_queues[s].size() == 0:
+
+        def avaialble_updates():
+            return [self._update_queues[f"({s},{self.index})"].size() for s in
+                    range(self._cluster.count(self.source))]
+
+        while np.sum(avaialble_updates()) == 0:
             print("waiting for update")
             time.sleep(1)
-        update = self._update_queues[s].dequeue()
-        for i in range(len(self.variables)):
-            name = self.variables[i].name
-            self.variables[i].assign(update[name])
-        print("Successfully pulled update")
+
+        updates = avaialble_updates()
+        for i in range(len(updates)):
+            if updates[i] > 0:
+                s = f"({i},{self.index})"
+                update = self._update_queues[s].dequeue()
+                for i in range(len(self.variables)):
+                    name = self.variables[i].name
+                    self.variables[i].assign(update[name])
+                print("Successfully pulled update")
+                break
--- test/test_distributed_model.py
+++ test/test_distributed_model.py
@@ -6,6 +6,14 @@ from dtf.cluster import Cluster
 from dtf.modules import DistributedModel
 import numpy as np
 
+physical_devices = tf.config.list_physical_devices('GPU')
+try:
+    for device in physical_devices:
+        tf.config.experimental.set_memory_growth(device, True)
+except:
+  # Invalid device or cannot modify virtual devices once initialized.
+  pass
+
 
 class DummyModel(tf.Module):
     def __init__(self):
@@ -31,7 +39,7 @@ def wait_for_shutdown(distributed_model):
             break
 
 def multisink_fn(args):
-    cluster_dict, name, task, source, sink = args
+    cluster_dict, name, task, source, sink, push_to_all = args
 
     clus = Cluster(cluster_dict, name, task)
     clus.start()
@@ -41,11 +49,14 @@ def multisink_fn(args):
                              source=source,
                              sink=sink,
                              mode=name,
-                             index=task,
-                             multi_sink=True)
+                             index=task, push_to_all=push_to_all)
     if name == source:
         model._base_model.v.assign(1.0)
-        model.push()
+        if push_to_all:
+            model.push()
+        else:
+            model.push(force_ind=0)
+            model.push(force_ind=1)
         wait_for_shutdown(model)
     else:
         assert model(5.0) == 0.0
@@ -65,7 +76,6 @@ def multisource_fn(args):
                              sink=sink,
                              mode=name,
                              index=task,
-                             multi_sink=False,
                              storage_spec={'dtypes': [tf.float32],
                                            'shapes': [(1,)],
                                            'names': ['test']})
@@ -89,19 +99,74 @@ def multisource_fn(args):
         assert 0 in found_updates
         assert 1 in found_updates
 
+def matrixed_fn(args):
+    cluster_dict, name, task, source, sink = args
+
+    clus = Cluster(cluster_dict, name, task)
+    clus.start()
+
+    model = DistributedModel(DummyModel(),
+                             cluster=clus,
+                             source=source,
+                             sink=sink,
+                             mode=name,
+                             index=task,
+                             storage_spec={'dtypes': [tf.float32],
+                                           'shapes': [(1,)],
+                                           'names': ['test']},
+                             push_to_all=True)
+    if name == source:
+        model.push({'test': np.array([task])})
+        wait_for_shutdown(model)
+    else:
+        found_updates = []
+        while True:
+            for s in model._update_queues:
+                if int(s[-2]) != task:
+                    continue
+                try:
+                    if model._update_queues[s].size() > 0:
+                        update = model._update_queues[s].dequeue()
+                        assert 'test' in update
+                        found_updates.append(update['test'].numpy()[0])
+                        print("Found update", task, found_updates)
+                except Exception as e:
+                    print(e)
+                    break
+            if len(found_updates) == clus.count(source):
+                break
+            time.sleep(1)
+        wait_for_shutdown(model)
+        print(found_updates)
+        assert 0 in found_updates
+        assert 1 in found_updates
+
+
 
 class TestDistributedModel(unittest.TestCase):
 
-    def test_multisink(self):
+    def test_multisink_push_to_one(self):
         cluster_dict = {'learner': ['localhost:6006'],
                         'worker': ['localhost:6007', 'localhost:6008']}
         with multiprocessing.Pool(3) as pool:
             pool.map(multisink_fn, [(cluster_dict, 'learner', 0,
-                                     'learner', 'worker'),
+                                     'learner', 'worker', False),
                                     (cluster_dict, 'worker', 0,
-                                     'learner', 'worker'),
+                                     'learner', 'worker', False),
                                     (cluster_dict, 'worker', 1,
-                                     'learner', 'worker')])
+                                     'learner', 'worker', False)])
+        print("Done")
+
+    def test_multisink_push_to_all(self):
+        cluster_dict = {'learner': ['localhost:6006'],
+                        'worker': ['localhost:6007', 'localhost:6008']}
+        with multiprocessing.Pool(3) as pool:
+            pool.map(multisink_fn, [(cluster_dict, 'learner', 0,
+                                     'learner', 'worker', True),
+                                    (cluster_dict, 'worker', 0,
+                                     'learner', 'worker', True),
+                                    (cluster_dict, 'worker', 1,
+                                     'learner', 'worker', True)])
         print("Done")
 
     def test_multisource(self):
@@ -114,6 +179,20 @@ class TestDistributedModel(unittest.TestCase):
                                      'worker', 'learner'),
                                     (cluster_dict, 'worker', 1,
                                      'worker', 'learner')])
+        print("Done")
+
+    def test_matrixed(self):
+        cluster_dict = {'learner': ['localhost:6005', 'localhost:6006'],
+                        'worker': ['localhost:6007', 'localhost:6008']}
+        with multiprocessing.Pool(4) as pool:
+            pool.map(matrixed_fn, [(cluster_dict, 'learner', 0,
+                                     'worker', 'learner'),
+                                      (cluster_dict, 'learner', 1,
+                                     'worker', 'learner'),
+                                      (cluster_dict, 'worker', 0,
+                                       'worker', 'learner'),
+                                      (cluster_dict, 'worker', 1,
+                                       'worker', 'learner')])
         print("Done")
 
 
